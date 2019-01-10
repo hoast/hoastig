1fe4cb21bef5b70dd715d7e1aa9781cad9af2d0a
Ron Dekker <redkenrok@gmail.com>
2019-01-10 13:12:43 +0100

    Added templates for test files for options

---
+++ test/options/noTransformJS.js
@@ -0,0 +1,9 @@
+// Dependency modules.
+const test = require(`ava`);
+// Custom module.
+//const Hoastig = require(`../../library`);
+
+test(`default`, function(t) {
+	// TODO: Finish test.
+	t.pass();
+});
